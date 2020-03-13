1722fbffb3e256d53c17f80e8003a93625c45883
Marta Zażlak <marta.zazlak@hotmail.com>
2020-03-13 12:36:58 +0100

    [ISSUE-6] /file-b endpoint

--- src/routing/file/index.js
+++ src/routing/file/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
+const { readDataFromTxtFile } = require('../../tools/TxtFileReader');
 
 module.exports = basePath => {
-    const DEFAULT_RESPONSE = fs.readFileSync(`${basePath}/file-a.txt`).toString();
+    const FILE_A_RESPONSE = fs.readFileSync(`${basePath}/file-a.txt`).toString();
 
-    router.get('/file-a', (req, res) => res.send(DEFAULT_RESPONSE));
+    router.get('/file-a', (req, res) => res.send(FILE_A_RESPONSE));
+
+    router.get('/file-b', (req, res) => {
+        const path = `${basePath}/file-b.txt`;
+        const callback = data => res.send(data).toString();
+        readDataFromTxtFile(path, callback);
+    });
 
     return router;
 };
